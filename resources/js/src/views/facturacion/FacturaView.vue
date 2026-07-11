0bc4089b10f152399f73a9eaeee35f4dd07142fe
proyectosr23w7 <proyectosr23w7@gmail.com>
2026-07-10 23:54:55 -0400

    Add direct invoice print action

--- resources/js/src/views/facturacion/FacturaView.vue
+++ resources/js/src/views/facturacion/FacturaView.vue
@@ -38,6 +38,7 @@ import {
     LoaderCircle,
     MoreVertical,
     Plus,
+    Printer,
     RotateCcw,
     Search,
     Send,
@@ -324,6 +325,9 @@ const changeScope = async (scope: string) => {
 };
 const downloadFile = (url?: string | null) => {
     if (url) window.open(url, '_blank', 'noopener,noreferrer');
+};
+const printFactura = (item: Factura) => {
+    downloadFile(String(item.pdf_download_url ?? ''));
 };
 const openSiatConsult = (item: Factura) => {
     const url = String(item.consulta_siat_url ?? '');
@@ -860,6 +864,18 @@ onMounted(store.load);
                                                 ><Eye
                                                     class="size-4"
                                                 />Detalle</Button
+                                            ><Button
+                                                v-if="
+                                                    capabilities.descargar &&
+                                                    item.pdf_download_url
+                                                "
+                                                variant="outline"
+                                                size="sm"
+                                                class="border-[#cbd8cf] text-[#126c3a]"
+                                                @click="printFactura(item)"
+                                                ><Printer
+                                                    class="size-4"
+                                                />Imprimir</Button
                                             ><DropdownMenu
                                                 ><DropdownMenuTrigger as-child
                                                     ><Button
